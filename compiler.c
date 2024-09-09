b48e07f002642a0941f1d64816d4745c19e3226b
lwcarani <lukecarani@gmail.com>
2024-09-08 21:15:12 -0400

    ch. 21

--- compiler.c
+++ compiler.c
@@ -171,6 +171,18 @@ static void declaration();
 static ParseRule *getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
+static uint8_t identifierConstant(Token *name)
+{
+    return makeConstant(OBJ_VAL(copyString(name->start,
+                                           name->length)));
+}
+
+static uint8_t parseVariable(const char *errorMessage)
+{
+    consume(TOKEN_IDENTIFIER, errorMessage);
+    return identifierConstant(&parser.previous);
+}
+
 static void binary()
 {
     TokenType operatorType = parser.previous.type;
@@ -246,8 +258,18 @@ static void number()
 
 static void string()
 {
-    emitConstant(OBJ_VAL(copyString(parser.previous.start + 1,
-                                    parser.previous.length - 2)));
+    emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
+}
+
+static void namedVariable(Token name)
+{
+    uint8_t arg = identifierConstant(&name);
+    emitBytes(OP_GET_GLOBAL, arg);
+}
+
+static void variable()
+{
+    namedVariable(parser.previous);
 }
 
 static void unary()
@@ -291,7 +313,7 @@ ParseRule rules[] = {
     [TOKEN_GREATER_EQUAL] = {NULL, binary, PREC_COMPARISON},
     [TOKEN_LESS] = {NULL, binary, PREC_COMPARISON},
     [TOKEN_LESS_EQUAL] = {NULL, binary, PREC_COMPARISON},
-    [TOKEN_IDENTIFIER] = {NULL, NULL, PREC_NONE},
+    [TOKEN_IDENTIFIER] = {variable, NULL, PREC_NONE},
     [TOKEN_STRING] = {string, NULL, PREC_NONE},
     [TOKEN_NUMBER] = {number, NULL, PREC_NONE},
     [TOKEN_AND] = {NULL, NULL, PREC_NONE},
@@ -332,18 +354,6 @@ static void parsePrecedence(Precedence precedence)
         ParseFn infixRule = getRule(parser.previous.type)->infix;
         infixRule();
     }
-}
-
-static uint8_t identifierConstant(Token *name)
-{
-    return makeConstant(OBJ_VAL(copyString(name->start,
-                                           name->length)));
-}
-
-static uint8_t parseVariable(const char *errorMessage)
-{
-    consume(TOKEN_IDENTIFIER, errorMessage);
-    return identifierConstant(&parser.previous);
 }
 
 static void defineVariable(uint8_t global)
